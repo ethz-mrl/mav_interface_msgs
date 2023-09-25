e86fda4a07be4c48cc59ace0ab93758e9f3af0d4
Sotiris Papatheodorou <sotiris@papatheodorou.xyz>
2023-09-25 19:34:47 +0200

    Rename FullStateTrajectory to Trajectory

--- include/mav_interface_msgs/conversions.h
+++ include/mav_interface_msgs/conversions.h
@@ -11,8 +11,8 @@
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 #include <mav_interface_msgs/FullStateStamped.h>
-#include <mav_interface_msgs/FullStateTrajectory.h>
 #include <mav_interface_msgs/Path.h>
+#include <mav_interface_msgs/Trajectory.h>
 #include <mav_interface_msgs/Waypoint.h>
 
 namespace mav_interface_msgs {
